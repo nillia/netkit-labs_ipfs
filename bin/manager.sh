6d39200468f7dacdb197cbb4b28c58c6b60adeb4
Nicolás Illia <nicoillia@gmail.com>
2017-12-04 20:29:42 -0300

    Improving args usage

--- bin/manager.sh
+++ bin/manager.sh
@@ -2,14 +2,24 @@
 
 init_all_nodes(){
   for NODE in "${NODES[@]}"; do
-	  run_remote $NODE "ipfs-init" &
+	  run_remote $NODE "ipfs-init $@" &
   done
 
-	sleep 5
+	sleep 10
 }
 
 stop_all_nodes(){
   for NODE in "${NODES[@]}"; do
 	  run_remote $NODE "ipfs-stop"
+  done
+}
+
+get_node_addr(){
+  run_remote "$1" ipfs id | grep "$1" | sed -e "s/[\",\,]//gi" -e "s/\s//gi"
+}
+
+remove_bootstrap(){
+  for NODE in "${NODES[@]}"; do
+	  run_remote $NODE "ipfs bootstrap rm --all" &
   done
 }
--- lab.sh
+++ lab.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 source bin/netkit.sh
+source bin/remote.sh
 source config/"$1".conf
 
 start_lab
-# TODO: Do something
-sleep 30
+
+for TEST in "${@:2}"; do
+  echo "Running test: $TEST"
+  run_remote "root@$MANAGER" 'bash $PATH_TESTS'/"$TEST.sh"
+done
+
 stop_lab
+
