efeef5349baf67aa5be45a4f531a057cbbdef568
Nicolás Illia <nicoillia@gmail.com>
2017-12-05 17:08:50 -0300

    Adding arguments to lab launcher

--- lab.sh
+++ lab.sh
@@ -4,12 +4,17 @@ source bin/netkit.sh
 source bin/remote.sh
 source config/"$1".conf
 
-start_lab
-
-for TEST in "${@:2}"; do
-  echo "Running test: $TEST"
-  run_remote "root@$MANAGER" 'bash $PATH_TESTS'/"$TEST.sh"
-done
-
-stop_lab
-
+case $2 in
+  start) start_lab
+  ;;
+  stop) stop_lab
+  ;;
+  test)
+    start_lab
+    for TEST in "${@:3}"; do
+      echo "Running test: $TEST"
+      run_remote "root@$MANAGER" 'bash $PATH_TESTS'/"$TEST.sh"
+    done
+    stop_lab
+  ;;
+esac
