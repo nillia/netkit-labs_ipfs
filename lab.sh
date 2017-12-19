cced7d1c932145d7115cc382f1dfb41fd25637f0
Nicolás Illia <nicoillia@gmail.com>
2017-12-19 15:59:13 -0300

    Improving lab launcher

--- lab.sh
+++ lab.sh
@@ -2,19 +2,35 @@
 
 source bin/netkit.sh
 source bin/remote.sh
-source config/"$1".conf
 
-case $2 in
-  start) start_lab
+import_lab(){
+  if [ -d "labs/$1" ]; then
+    source config/"$1".conf
+  else
+    echo "Cannot find lab: $1"
+    echo "Availables: "
+    cd labs && find * -prune -type d -exec echo "- "{} \; && cd --
+    exit 1
+   fi
+}
+
+import_lab "$2"
+
+case $1 in
+  start)
+    start_lab
   ;;
-  stop) stop_lab
+  stop)
+    stop_lab
   ;;
   test)
-    start_lab
     for TEST in "${@:3}"; do
       echo "Running test: $TEST"
       run_remote "root@$MANAGER" 'bash $PATH_TESTS'/"$TEST.sh"
     done
-    stop_lab
   ;;
+  *)
+      echo "Usage: "
+      echo "lab.sh [start | stop] lab_id"
+      echo "lab.sh test lab_id test_id [test_id..]"
 esac
